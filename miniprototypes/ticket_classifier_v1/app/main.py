71c30733e703c5e4bad7dfbff51eb87ad0503aa5
Cristina <cristinaguerreroflores@gmail.com>
2025-06-02 13:27:19 +0200

    Adding miniprototypes

--- miniprototypes/ticket_classifier_v1/app/main.py
+++ miniprototypes/ticket_classifier_v1/app/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
+
 from app.schema import TicketRequest, TicketResponse
 from app.model import predict
 
+
 app = FastAPI()
+
 
 @app.get("/")
 def root():
     return {"message": "Ticket Classifier API is live"}
+
 
 @app.post("/predict", response_model=TicketResponse)
 def classify_ticket(req: TicketRequest):
